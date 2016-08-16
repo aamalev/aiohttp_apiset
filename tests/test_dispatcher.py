16708941308024eb486c6a7f0981935ac10b50c8
Alexander Malev <yttrium@somedev.ru>
2016-08-16 23:40:19 +0300

    use make_mocked_request

--- tests/test_dispatcher.py
+++ tests/test_dispatcher.py
@@ -1,18 +1,11 @@
 import asyncio
-from unittest import mock
 
 import pytest
 from aiohttp import web
 from aiohttp.web_urldispatcher import MatchInfoError
+from aiohttp.test_utils import make_mocked_request as make_request
 
 from aiohttp_apiset.dispatcher import TreeUrlDispatcher
-
-
-@pytest.fixture
-def request():
-    request = mock.MagicMock()
-    request.method = 'GET'
-    return request
 
 
 def handler(request):
@@ -37,28 +30,28 @@ def test_create(dispatcher: TreeUrlDispatcher):
 
 @asyncio.coroutine
 def test_simple(dispatcher: TreeUrlDispatcher, request: web.Request):
-    request.raw_path = '/api/1/pet/1'
+    request = make_request('GET', '/api/1/pet/1')
     md = yield from dispatcher.resolve(request)
     assert md == {'id': '1'}
 
-    request.raw_path = '/api/1/pets'
+    request = make_request('GET', '/api/1/pets')
     md = yield from dispatcher.resolve(request)
     assert not md
 
 
 @asyncio.coroutine
-def test_multisubs(dispatcher: TreeUrlDispatcher, request: web.Request):
+def test_multisubs(dispatcher: TreeUrlDispatcher):
     url = '/api/1/host/{host}/eth{num}/{ip:[.\d]+}/'
     dispatcher.add_route('GET', url, handler)
 
-    request.raw_path = '/api/1/host/myhost/eth0/127.0.0.1/'
+    request = make_request('GET', '/api/1/host/myhost/eth0/127.0.0.1/')
     md = yield from dispatcher.resolve(request)
     assert len(md)
     assert 'ip' in md
     assert 'num' in md
     assert 'host' in md
 
-    request.raw_path = '/api/1/host/myhost/eth0/127.0.0.1'
+    request = make_request('GET', '/api/1/host/myhost/eth0/127.0.0.1')
     md = yield from dispatcher.resolve(request)
     assert isinstance(md, MatchInfoError)
 
