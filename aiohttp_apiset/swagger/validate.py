d9ebd65faf29edf1df8d58bc8f74494449b4d8f8
Alexander Malev <yttrium@somedev.ru>
2016-10-27 22:45:08 +0300

    fix convert

--- aiohttp_apiset/swagger/validate.py
+++ aiohttp_apiset/swagger/validate.py
@@ -1,6 +1,6 @@
 from jsonschema import validate
 
-ERROR_TYPE = 'Not valid value "{}" for type {}:{}'
+ERROR_TYPE = "Not valid value '{}' for type {}:{}"
 
 
 def to_bool(value):
@@ -21,8 +21,6 @@ types_mapping = {
         'float': float,
         'double': float,
     },
-    'string': lambda x: x,
-    'file': lambda x: x,
 }
 
 
@@ -32,23 +30,24 @@ def convert(name, value, sw_type, sw_format, errors):
     if isinstance(conv, dict):
         conv = conv.get(sw_format, conv[None])
 
-    if sw_type != 'string':
-        if isinstance(value, (list, tuple)):
-            result = []
-            for i, v in enumerate(value):
-                try:
-                    result.append(conv(v))
-                except (ValueError, TypeError):
-                    errors.add(
-                        '{}.{}'.format(name, i),
-                        ERROR_TYPE.format(v, sw_type, sw_format)
-                    )
-            return result
-        else:
+    if sw_type in ('string', 'file'):
+        return value
+    elif isinstance(value, (list, tuple)):
+        result = []
+        for i, v in enumerate(value):
             try:
-                return conv(value)
+                result.append(conv(v))
             except (ValueError, TypeError):
                 errors.add(
-                    name,
-                    ERROR_TYPE.format(value, sw_type, sw_format)
+                    '{}.{}'.format(name, i),
+                    ERROR_TYPE.format(v, sw_type, sw_format)
                 )
+        return result
+    else:
+        try:
+            return conv(value)
+        except (ValueError, TypeError):
+            errors.add(
+                name,
+                ERROR_TYPE.format(value, sw_type, sw_format)
+            )
