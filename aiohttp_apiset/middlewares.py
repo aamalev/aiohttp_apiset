43bc5ca9e41783f82cae07437c7fe74b65a54d9e
Alexander Malev <yttrium@somedev.ru>
2016-10-27 22:49:01 +0300

    refactor middleware + JsonEncoder

--- aiohttp_apiset/middlewares.py
+++ aiohttp_apiset/middlewares.py
@@ -1,17 +1,38 @@
 import asyncio
 import json
 
-from aiohttp import web
+from aiohttp import web, multidict
+
+
+class JsonEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, multidict.MultiDict):
+            return {k: o.getall(k) for k in o}
+        return super().default(o)
+
+    @classmethod
+    def dumps(cls, *args, **kwargs):
+        kwargs.setdefault('cls', cls)
+        return json.dumps(*args, **kwargs)
 
 
 @asyncio.coroutine
 def jsonify(app, handler):
-    dumps = app.get('dumps', json.dumps)
+    dumps = app.get('dumps', JsonEncoder.dumps)
 
     @asyncio.coroutine
     def process(request):
         try:
             response = yield from handler(request)
+        except web.HTTPException as ex:
+            if not isinstance(ex.reason, str):
+                return web.json_response(
+                    {'errors': ex.reason}, status=ex.status, dumps=dumps)
+            elif ex.status > 399:
+                return web.json_response(
+                    {'error': ex.reason}, status=ex.status, dumps=dumps)
+            raise
+        else:
             if isinstance(response, dict):
                 status = response.get('status', 200)
                 if not isinstance(status, int):
@@ -21,12 +42,4 @@ def jsonify(app, handler):
             elif not isinstance(response, web.StreamResponse):
                 return web.json_response(response, dumps=dumps)
             return response
-        except web.HTTPException as ex:
-            if not isinstance(ex.reason, str):
-                return web.json_response(
-                    ex.reason, status=ex.status, dumps=dumps)
-            elif ex.status > 399:
-                return web.json_response(
-                    {'error': ex.reason}, status=ex.status, dumps=dumps)
-            raise
     return process
