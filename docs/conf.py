e1ff06e6e41d0348fd2292c06e1bb5f06f3bd8a5
Alexander Malev <yttrium@somedev.ru>
2016-11-06 23:46:35 +0300

    comment sphinx.ext.githubpages

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
+    # 'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
