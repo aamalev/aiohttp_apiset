d71963884c0b0f6509a3ebea632e6e7a7ba3fd9d
Alexander Malev <yttrium@somedev.ru>
2016-11-06 23:59:48 +0300

    parent path for docstrings

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 # -- General configuration ------------------------------------------------
 
