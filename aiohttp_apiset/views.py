4bf96cf76220b27729688e728ef531515f8c4c21
Alexander Malev <yttrium@somedev.ru>
2016-12-15 03:20:07 +0300

    trim views

--- aiohttp_apiset/views.py
+++ aiohttp_apiset/views.py
@@ -1,13 +1,6 @@
-import asyncio
 import functools
-import inspect
 import json
-import os
 import sys
-
-from aiohttp import web, abc
-from aiohttp.web import Application
-import yaml
 
 from .swagger.loader import SwaggerLoaderMixin
 from . import utils
@@ -16,27 +9,7 @@ PY_35 = sys.version_info >= (3, 5)
 
 
 class BaseApiSet:
-    @classmethod
-    def append_routes_to(cls, app: Application, prefix=None):
-        raise NotImplementedError()
-
-
-def swagger_yaml(file_path, *, executor=None, loop=None):
-    if loop is None:
-        loop = asyncio.get_event_loop()
-
-    def response(request):
-        with open(file_path) as f:
-            data = yaml.load(f)
-            return ApiSet.response_json(data)
-
-    @asyncio.coroutine
-    def aresponse(request):
-        return (
-            yield from loop.run_in_executor(
-                executor, response, request))
-
-    return aresponse
+    pass
 
 
 class ApiSet(BaseApiSet, SwaggerLoaderMixin):
@@ -88,98 +61,3 @@ class ApiSet(BaseApiSet, SwaggerLoaderMixin):
             u = utils.url_normolize(prefix + postfix)
             name = utils.to_name(cls.namespace + postfix)
             router.add_route('*', u, view, name=name)
-
-    @classmethod
-    def get_swagger_paths(cls):
-        return cls.get_sub_swagger('paths')
-
-    @property
-    def loop(self):
-        return self.app.loop
-
-    @asyncio.coroutine
-    def options(self, request):
-        return web.Response(body=b'')
-
-    @asyncio.coroutine
-    def data(self, request) -> dict:
-        if 'json' in request.content_type:
-            data = yield from request.json()
-        else:
-            data = yield from request.post()
-        return data
-
-    @asyncio.coroutine
-    def swagger(self, request):
-        module_path = sys.modules[self.__module__].__file__
-        swagger_file = os.path.join(
-            os.path.dirname(module_path), self.swagger_file)
-        return (yield from swagger_yaml(swagger_file)(request))
-
-    def add_swagger_route(self, router, prefix=None, namespace=None):
-        router.add_route(
-            'GET',
-            prefix + str(self.version) + self.docs + self.namespace,
-            self.swagger,
-            name=':'.join((namespace, 'swagger')),
-        )
-
-    def add_action_routes(self, router, prefix=None, namespace=None):
-        url = prefix + str(self.version) + '/' + self.namespace + '/'
-        for action_name, method, postfix_url in self.actions:
-            action = getattr(self, action_name, None)
-            if action:
-                if method == 'OPTIONS':
-                    name = None
-                else:
-                    name = ':'.join((namespace, action_name))
-                router.add_route(method, url + postfix_url, action, name=name)
-
-    def get_routes(self, base_url, namespace=None):
-        if not namespace:
-            namespace = namespace or self.namespace
-            namespace = namespace.replace('/', '.')
-
-        for action_name, method, postfix_url in self.actions:
-            action = getattr(self, action_name, None)
-            if action:
-                yield {
-                    'method': method,
-                    'path': base_url + postfix_url,
-                    'handler': action,
-                    'name': ':'.join((namespace, action_name)),
-                    'swagger_path': self.swagger_path,
-                }
-
-    @classmethod
-    def append_routes_to(cls, app: Application, prefix=None):
-        self = cls(app)
-        prefix = prefix or self._prefix
-        namespace = self.namespace.replace('/', '.')
-        namespace = namespace.replace('{', '')
-        namespace = namespace.replace('}', '')
-        self.add_swagger_route(app.router, prefix, namespace)
-        self.add_action_routes(app.router, prefix, namespace)
-
-    @classmethod
-    def response(cls, data=None, **kwargs):
-        response = getattr(cls, 'response_' + cls.default_response)
-        if isinstance(data, dict):
-            data = data.copy()
-        elif not data:
-            data = {}
-        elif isinstance(data, (map, list, set, tuple)):
-            return response(data, **kwargs)
-        else:
-            data = {'data_text': str(data)}
-        data.update(kwargs)
-        return response(data, **kwargs)
-
-    @classmethod
-    def response_json(cls, data, **kwargs):
-        return web.json_response(
-            data,
-            content_type='application/json; charset=utf-8',
-            status=kwargs.get('status', 200),
-            dumps=cls.dumps,
-        )
