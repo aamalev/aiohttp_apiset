815d95ffa2002142610364e5e424d3de0601ba2e
Alexander Malev <yttrium@somedev.ru>
2017-01-07 00:12:19 +0300

    use static prefix in swagger-ui template

--- aiohttp_apiset/swagger/ui.py
+++ aiohttp_apiset/swagger/ui.py
@@ -14,6 +14,7 @@ def get_template():
         return f.read()
 
 
-def rend_template(url):
+def rend_template(url, prefix=''):
     template = get_template()
-    return template.replace('{{url}}', url)
+    template = template.replace('{{url}}', url)
+    return template.replace('{{static_prefix}}', prefix)
