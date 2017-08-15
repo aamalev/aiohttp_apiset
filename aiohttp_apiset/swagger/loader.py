c4567d2d92ec621b389443700f6a24ec664e9081
Alexander Malev <yttrium@somedev.ru>
2017-08-16 00:16:53 +0300

    Schema loader infrastructure

--- aiohttp_apiset/swagger/loader.py
+++ aiohttp_apiset/swagger/loader.py
@@ -1,8 +1,16 @@
 import json
 import os
 import sys
+from collections import Mapping
+from functools import reduce
+from itertools import chain
+from pathlib import Path
 
 import yaml
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
 
 
 class SwaggerLoaderMixin:
@@ -82,15 +90,309 @@ class SwaggerLoaderMixin:
                 return default
         return data
 
-    @property
-    def swagger_path(self):
-        sw_prop = '_swagger_path'
-        data = getattr(self, sw_prop, None)
-        if data:
+
+class SchemaPointer(Mapping):
+    def __init__(self, schema_file, data):
+        self._file = schema_file
+        if '$ref' in data:
+            self._data = schema_file(data['$ref'])
+        else:
+            self._data = data
+
+    @classmethod
+    def factory(cls, f, data):
+        if isinstance(data, dict):
+            return cls(f, data)
+        elif isinstance(data, list):
+            return [cls.factory(f, i) for i in data]
+        else:
             return data
-        fp, path = self.get_swagger_filepath()
-        data = self.get_sub_swagger(path)
-        setattr(self, sw_prop, data)
+
+    def __getitem__(self, key):
+        if key not in self._data:
+            raise KeyError((key, self._data))
+        return self.factory(self._file, self._data[key])
+
+    def copy(self) -> dict:
+        def conv(x):
+            if isinstance(x, SchemaPointer):
+                return x.copy()
+            elif isinstance(x, list):
+                return [conv(o) for o in x]
+            else:
+                return x
+        return {k: conv(v) for k, v in self.items()}
+
+    def __iter__(self):
+        yield from self._data
+
+    def __len__(self):
+        return len(self._data)
+
+
+class AllOf(Mapping):
+    def __init__(self, *pointers):
+        self._pointers = pointers
+
+    def __getitem__(self, key):
+        for data in self._pointers:
+            if key in data:
+                return data[key]
+        raise KeyError(key)
+
+    def __iter__(self):
+        dub = set()
+        for data in self._pointers:
+            for i in data:
+                if i not in dub:
+                    dub.add(i)
+                    yield i
+
+    def __len__(self):
+        return len(reduce(set.union, self._pointers, set()))
+
+
+class SchemaFile(Mapping):
+    files = {}
+
+    def __new__(cls, path, *args, **kwargs):
+        if path in cls.files:
+            return cls.files[path]
+        else:
+            inst = super().__new__(cls)
+            inst.__init__(path, *args, **kwargs)
+            cls.files[inst.path] = inst
+            return inst
+
+    def __init__(self, path):
+        self._path = path
+        with path.open() as f:
+            self._data = yaml.load(f, Loader)
+
+    @property
+    def path(self):
+        return self._path
+
+    def find_path(self, path):
+        if isinstance(path, str):
+            s = path
+            path = Path(path)
+        else:
+            s = ''
+
+        if s.startswith('.'):
+            pass
+        elif path.exists() or path.is_absolute():
+            return path
+        return self._path.parent / path
+
+    def factory(self, path):
+        path = self.find_path(path)
+        if path in self.files:
+            return self.files[path]
+        return type(self)(path)
+
+    def __getitem__(self, item):
+        if item in self._data:
+            data = self._data[item]
+        else:
+            raise KeyError((item, self._data))
+        return SchemaPointer.factory(self, data)
+
+    def resolve_uri(self, uri):
+        sharp = uri.find('#')
+        if sharp >= 0:
+            path, rel_path = uri.split('#', 1)
+        else:
+            path, rel_path = uri, ''
+
+        if path:
+            file = self.factory(path)
+        else:
+            file = self
+
+        if not rel_path:
+            rel_path = ()
+        elif rel_path.startswith('/'):
+            rel_path = rel_path.split('/')[1:]
+        else:
+            raise NotImplementedError(uri)
+
+        return file, rel_path
+
+    def __call__(self, item):
+        data, rel_path = self.resolve_uri(item)
+
+        pointer_file = data
+        if not rel_path:
+            return data
+        for p in rel_path:
+            try:
+                data = data[p]
+            except KeyError:
+                raise KeyError(item)
+        if not isinstance(data, Mapping):
+            return data
+        return SchemaPointer(pointer_file, data)
+
+    def __len__(self):
+        return len(self._data)
+
+    def __iter__(self):
+        yield from self._data
+
+
+class IncludeSwaggerPaths(SchemaPointer):
+    INCLUDE = '$include'
+
+    @classmethod
+    def _get_includes(cls, methods):
+        if isinstance(methods, list):
+            return (o[cls.INCLUDE] for o in methods if cls.INCLUDE in o)
+        elif not isinstance(methods, dict):
+            return ()
+        elif cls.INCLUDE in methods:
+            return methods[cls.INCLUDE],
+        else:
+            return ()
+
+    def items(self):
+        for pref, methods in self._data.items():
+            if '$ref' in methods:
+                yield pref, self._file(methods['$ref'])
+                continue
+            includes = self._get_includes(methods)
+            if not includes:
+                yield pref, SchemaPointer.factory(self._file, methods)
+                continue
+            for i in includes:
+                f = self._file(i)
+                basePath = f.get('basePath', '')
+                for p, op in f['paths'].items():
+                    yield pref + basePath + p, op
+
+    def __iter__(self):
+        for uri, methods in self.items():
+            yield uri
+
+    def __getitem__(self, item):
+        data = sorted(self._data.items(), key=lambda x: -len(x[0]))
+        for pref, methods in data:
+            if item.startswith(pref):
+                if '$ref' in methods and len(pref) == len(item):
+                    return self._file(methods['$ref'])
+                includes = self._get_includes(methods)
+                if not includes and len(pref) == len(item):
+                    return methods
+                subitem = item[len(pref):]
+                for i in includes:
+                    f = self._file(i)
+                    basePath = f.get('basePath', '')
+                    i = subitem[len(basePath):]
+                    try:
+                        return f['paths'][i]
+                    except KeyError:
+                        pass
+        raise KeyError(item)
+
+    def __len__(self):
+        raise NotImplementedError
+
+
+class ExtendedSchemaFile(SchemaFile):
+    include = IncludeSwaggerPaths
+    files = {}
+
+    def __init__(self, path: Path, dirs: list=()):
+        self._dirs = dirs
+        self._cache = {}
+        super().__init__(self.find_path(path))
+        self._ref_replaced = False
+
+    def __getitem__(self, item):
+        if item == 'paths':
+            return self.include(self, self._data['paths'])
+        return super().__getitem__(item)
+
+    def factory(self, path):
+        path = self.find_path(path)
+        if path in self.files:
+            return self.files[path]
+        return type(self)(path, self._dirs)
+
+    def find_path(self, path):
+        if path in self._cache:
+            return self._cache[path]
+        else:
+            key = path
+        dirs = []
+        if isinstance(path, str):
+            if path.startswith('.'):
+                dirs.append(self._path.parent)
+            path = Path(path)
+        elif path.parts[0].startswith('.'):
+            dirs.append(self._path.parent)
+
+        if dirs:
+            pass
+        elif path.exists():
+            path = Path(os.path.normpath(str(path)))
+            self._cache[key] = path
+            return path
+        elif path.is_absolute():
+            raise FileNotFoundError(path)
+
+        for i in chain(dirs, self._dirs):
+            p = i / path
+            if p.exists():
+                p = Path(os.path.normpath(str(p)))
+                self._cache[key] = p
+                return p
+        raise FileNotFoundError(path)
+
+    def resolve(self):
+        data = self._replace_reference(self._data)
+        paths = {}
+        for pref, methods in data['paths'].items():
+            includes = self.include._get_includes(methods)
+            if not includes:
+                paths[pref] = methods
+                continue
+            for i in includes:
+                sub = self.factory(i).resolve()
+                basePath = sub.get('basePath', '')
+                for uri, methods in sub['paths'].items():
+                    paths[pref + basePath + uri] = methods
+        data['paths'] = paths
+        return data
+
+    def _replace_reference(self, data):
+        if self._ref_replaced and data is self._data:
+            return data
+        self._ref_replaced = True
+
+        if not isinstance(data, (dict, list)):
+            return data
+
+        is_dict = isinstance(data, dict)
+
+        if is_dict and '$ref' in data:
+            f, rel = self.resolve_uri(data['$ref'])
+
+            if f is self:
+                data = self._data
+            else:
+                data = f._replace_reference(f._data)
+
+            for p in rel:
+                data = data[p]
+            return data
+
+        gen = data.items() if is_dict else enumerate(data)
+        for k, v in gen:
+            new_v = self._replace_reference(v)
+            if new_v is not v:
+                data[k] = new_v
         return data
 
 
@@ -131,3 +433,32 @@ def deref(data, spec: dict):
                 result = data[:]
             result[k] = new_v
     return result or data
+
+
+class BaseLoader:
+    def __init__(self, search_dirs=()):
+        self._search_dirs = list(search_dirs)
+
+    @property
+    def search_dirs(self):
+        return self._search_dirs
+
+    def add_search_dir(self, search_dir):
+        self._search_dirs.append(Path(search_dir))
+
+    def load(self, path):
+        raise NotImplementedError
+
+
+class FileLoader(BaseLoader):
+    file_factory = ExtendedSchemaFile
+
+    def load(self, path):
+        f = self.file_factory(path, dirs=self._search_dirs)
+        return f
+
+
+class DictLoader(FileLoader):
+    def load(self, path):
+        f = super().load(path)
+        return f.resolve()
---
+++ tests/test_schema_file.py
@@ -0,0 +1,70 @@
+from pathlib import Path
+
+import pytest
+
+from aiohttp_apiset.swagger.loader import ExtendedSchemaFile, SchemaFile
+from aiohttp_apiset.swagger.loader import DictLoader, FileLoader, AllOf
+
+
+def test_load():
+    p = Path(__file__).parent / 'data/schema01.yaml'
+    f = SchemaFile(p)
+    assert f is SchemaFile(p)
+    assert f('../data/file.yaml#/basePath') == '/image'
+    assert 'get' in f['paths']['/pet']
+
+
+@pytest.mark.parametrize('p', [
+    'data/schema01.yaml',
+    'data/root.yaml',
+])
+def test_paths(p):
+    d = Path(__file__).parent
+    f = ExtendedSchemaFile(p, [d, d / 'data'])
+    paths = f['paths']
+    items = list(paths.items())
+    assert len(items) == len([url for url in paths])
+    for url, m in items:
+        methods = paths[url]
+        assert dict(m) == dict(methods)  # , pytest.set_trace()
+
+
+@pytest.mark.parametrize('p', [
+    'data/schema01.yaml',
+    'data/root.yaml',
+])
+def test_resolve(p):
+    d = Path(__file__).parent
+    f = ExtendedSchemaFile(p, [d, d / 'data'])
+    data = f.resolve()
+    assert data['paths']
+
+
+def test_route_include(swagger_router):
+    paths = [route.url_for().human_repr()
+             for route in swagger_router.routes()]
+    assert '/api/1/include2/inc/image' in paths, paths
+
+
+@pytest.mark.parametrize('loader', [
+    FileLoader,
+    DictLoader,
+])
+@pytest.mark.parametrize('p', [
+    'data/schema01.yaml',
+    'data/root.yaml',
+])
+def test_loader(loader, p):
+    l = loader()
+    d = Path(__file__).parent
+    l.add_search_dir(d)
+    l.add_search_dir(d / 'data')
+    assert l.load(p)
+
+
+def test_allOf():
+    a = AllOf({'a': 1}, {'b': 2})
+    assert dict(a) == {'a': 1, 'b': 2}
+    with pytest.raises(KeyError):
+        print(a['c'])
+    assert len(a) == 2
