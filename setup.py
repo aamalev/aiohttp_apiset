fd7d3f1e478d7cd738d35f3bcace26605e1766ab
Alexander Malev <yttrium@somedev.ru>
2017-12-22 03:21:54 +0300

    update versions

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 else:
     if not os.path.exists(swagger_ui.STATIC_DIR):
         swagger_ui.setup_ui('2.2.10')
-        swagger_ui.setup_ui('3.4.0')
+        swagger_ui.setup_ui('3.7.0')
 
 
 with open(os.path.join(os.path.abspath(os.path.dirname(
@@ -27,7 +27,7 @@ def read(f):
 
 
 install_requires = [
-    'aiohttp>=1.2',
+    'aiohttp>=1.2,<=2.3.6',
     'pyyaml',
     'jsonschema',
     'yarl',
