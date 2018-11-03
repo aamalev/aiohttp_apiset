504c9b3501b7c8a2b3e6324c1f5305c6f7cc2bad
Alexander Malev <aamalev@users.noreply.github.com>
2018-11-03 11:56:09 +0300

    Programming Language :: Python :: 3.7

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP'],
     author='Alexander Malev',
     author_email='malev@somedev.ru',
